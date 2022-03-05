118bf083a8dc98ae5ef6441f14ea067dbe2409ce
Helmetdude117 <93328530+Helmetdude117@users.noreply.github.com>
2022-03-05 13:42:18 -0300

    funcao de ler arquivo

--- mainprog.py
+++ mainprog.py
@@ -1,1 +1,8 @@
-#Trbalho covid
+#Trabalho covid
+#!!!!!Usando tabela temporaria pq arquivo ta muito grande heuheueh
+
+def arvivoLista():
+    list = []
+    arquivo = open('tabelatemp.txt', 'r')
+    for line in arquivo:
+        list.append(line)
