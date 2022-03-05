5e615581bf2480b685361f61f9e7c90f1af49724
Helmetdude117 <93328530+Helmetdude117@users.noreply.github.com>
2022-03-05 13:47:13 -0300

    g

--- mainprog.py
+++ mainprog.py
@@ -1,8 +1,12 @@
 #Trabalho covid
 #!!!!!Usando tabela temporaria pq arquivo ta muito grande heuheueh
 
-def arvivoLista():
-    list = []
-    arquivo = open('tabelatemp.txt', 'r')
-    for line in arquivo:
-        list.append(line)
+def arquivoLista():
+    lines = []
+    with open("tabelatemp.txt") as file:
+        for line in file: 
+            line = line.strip() #or some other preprocessing
+            lines.append(line) #storing everything in memory!
+    return lines
+        
+print(arquivoLista())
