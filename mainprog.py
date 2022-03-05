57613b30c857c1f222c14031ee7a1dd271a0139f
Helmetdude117 <93328530+Helmetdude117@users.noreply.github.com>
2022-03-05 17:37:29 -0300

    criacao de listas
    basicamente um bando de funcaso que faz listas dependendo sde certas caracteristicas q vai ajudar a separar p fazer as questoes mais p frente

--- mainprog.py
+++ mainprog.py
@@ -9,23 +9,67 @@ def arquivoLista():
             lines.append(line)
         del lines [0]
     return lines
-        
-print(arquivoLista())
 
 def porcentagem(parte,inteiro):
     percentual = ( int(parte) / int(inteiro) ) * 100
     return percentual
 
-def quantidadePessoas(list):
+def calculoQtd(list):
     quantity = 0
     for item in list:
         quantity += 1
     return quantity
 
-def quantidadeMasc(list):
-    quantity = 0 
-    for item in list:
+def listaHomens(lista):
+    homens = []
+    for item in lista:
         if 'Masculino' in item:
-            quantity += 1
-    return quantity
-    
+            homens.append(item)
+    return homens
+
+def listaMulheres(lista):
+    mulheres = []
+    for item in lista:
+        if 'Feminino' in item:
+            mulheres.append(item)
+    return mulheres
+
+def listaPositivo(lista):
+    final = []
+    for item in lista:
+        if 'Positivo' in item:
+            final.append(item)
+    return final
+
+def listaAssintomaticos(lista):
+    final = []
+    for item in lista:
+        if 'Assintomático' in item:
+            final.append(item)
+    return final
+
+def listaSintomaticos(lista):
+    final = []
+    for item in lista:
+        if 'Assintomatico' not in item:
+            final.append(item)
+    return final
+
+def acima50(lista):
+    final = [] 
+    for item in lista:
+        number = item[-2]
+        if int(number) >= 5:
+            final.append(item)
+    return final
+
+def abaixo20(lista):
+    final = [] 
+    for item in lista:
+        number = item[-2] + item[-1]
+        if int(number) <= 20:
+            final.append(item)
+    return final
+
+Lista = arquivoLista()
+print(abaixo20(Lista))
