3d4b64d38e085cfb578ae6b65f83a8bff518c96b
Helmetdude117 <93328530+Helmetdude117@users.noreply.github.com>
2022-03-05 16:25:10 -0300

    adicionando funcoes

--- mainprog.py
+++ mainprog.py
@@ -5,9 +5,27 @@ def arquivoLista():
     lines = []
     with open("tabelatemp.txt",'r') as file:
         for line in file: 
-            
-            lines.append(line) #storing everything in memory!
+            line = line.replace('\n', '')
+            lines.append(line)
+        del lines [0]
     return lines
         
 print(arquivoLista())
 
+def porcentagem(parte,inteiro):
+    percentual = ( int(parte) / int(inteiro) ) * 100
+    return percentual
+
+def quantidadePessoas(list):
+    quantity = 0
+    for item in list:
+        quantity += 1
+    return quantity
+
+def quantidadeMasc(list):
+    quantity = 0 
+    for item in list:
+        if 'Masculino' in item:
+            quantity += 1
+    return quantity
+    
