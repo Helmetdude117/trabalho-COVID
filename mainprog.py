5351aba4a1111aa358cb5d81e3e2e4755fb070b3
Helmetdude117 <93328530+Helmetdude117@users.noreply.github.com>
2022-03-06 18:42:50 -0300

    funcao check duas listas

--- mainprog.py
+++ mainprog.py
@@ -57,6 +57,13 @@ def NOTfinder(lista,parametro):
             final.append(item)
     return final
 
+def repetidos(lista1, lista2):
+    final = []
+    for i in range(len(lista1)):
+        for j in range(len(lista2)):
+            if lista1[i] == lista2[j]:
+                final.append(lista1[i])
+
 def main():
     Lista = arquivoLista()
     Total_pessoas = calculoQtd(Lista)
